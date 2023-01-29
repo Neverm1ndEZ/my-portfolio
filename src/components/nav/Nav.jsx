a9ef9a87f953c2b94f775b23ec3f72dd7e44ac35
Mihir Suman <mihirsuman0131@gmail.com>
2023-01-29 06:05:43 +0530

    minor-changes_1

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
 
   return (
     <nav>
-      <a href="/#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#header" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experiences')} className={activeNav === '#experiences' ? 'active' : ''}><SiKnowledgebase /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageRoundedDetail /></a> 
